7d064860015e127078c606ff02341d7c316f704b
verzinonly1 <verzinonly@gmail.com>
2025-11-27 07:55:32 +0100

    Create user.js with user profile messages

---
+++ src/modules/messages/user.js
@@ -0,0 +1,23 @@
+// user.js 
+
+const userProfile = {
+    profileInfo: {
+        name: 'User Name',
+        age: 25,
+        bio: 'This is a user bio'
+    },
+    warnings: [
+        'First warning: Spamming messages.',
+        'Second warning: Inappropriate language.'
+    ],
+    bans: [
+        'First ban: Violation of rules.',
+        'Second ban: Harassment.'
+    ],
+    anonymousMessages: [
+        'I think you are great!',
+        'Keep up the good work!'
+    ]
+};
+
+module.exports = userProfile;
